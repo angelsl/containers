7749bb96377ee3719a418d39bcb2dd554238ee1b
angelsl <angelsl@in04.sg>
2023-08-02 01:43:00 +0800

    Update setup-fireflyiii.sh

--- setup-fireflyiii.sh
+++ setup-fireflyiii.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+curl -L "https://getcomposer.org/download/2.5.8/composer.phar" > /tmp/composer.phar
+sha256sum -c <(cat <<EOF
+f07934fad44f9048c0dc875a506cca31cc2794d6aebfc1867f3b1fbf48dce2c5 /tmp/composer.phar
+EOF
+)
+
 mkdir -p "${ARG_DEST}"
 cd "${ARG_DEST}"
 
@@ -20,8 +26,8 @@ else
   git clone --depth 1 -b "${ARG_REF}" "${ARG_GIT_REMOTE}" .
 fi
 
-composer install --no-scripts --no-dev --ignore-platform-reqs
-composer install --no-dev --ignore-platform-reqs
+php /tmp/composer.phar install --no-scripts --no-dev --ignore-platform-reqs
+php /tmp/composer.phar install --no-dev --ignore-platform-reqs
 
 if [ -n "${ARG_ENV_LINK:-}" ]; then
   rm .env
