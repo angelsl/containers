ba95d258d7d27432f8bacb7af18427254fd77358
angelsl <angelsl@in04.sg>
2023-08-03 01:17:25 +0800

    Update

---
+++ postgres-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if ! locale -a | grep -q en_SG; then
+  localedef -i en_SG -c -f UTF-8 -A /usr/share/locale/locale.alias en_SG.UTF-8
+fi
+
+export LANG=en_SG.UTF-8
+exec /usr/local/bin/docker-entrypoint.sh "$@"
