18c2ee68bc05bdf3b1d4ee4871d103c0374f14c2
angelsl <angelsl@in04.sg>
2023-08-03 01:30:01 +0800

    Update postgres-entrypoint

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -7,4 +7,4 @@ if ! locale -a | grep -q en_SG; then
 fi
 
 export LANG=en_SG.UTF-8
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+exec /usr/local/bin/gosu postgres:postgres /usr/local/bin/docker-entrypoint.sh "$@"
