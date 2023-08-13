143c2ef4634cdf8c341cf69df845d8ac837577c8
angelsl <angelsl@in04.sg>
2023-08-14 00:48:48 +0800

    Remove pg_back

--- postgres-entrypoint.sh
+++ postgres-entrypoint.sh
@@ -7,13 +7,4 @@ if ! locale -a | grep -q en_SG; then
 fi
 
 export LANG=en_SG.UTF-8
-
-if [ ! -f /usr/local/bin/pg_back ]; then
-  curl -qsSL 'https://github.com/orgrim/pg_back/releases/download/v2.1.1/pg_back_2.1.1_linux_amd64.tar.gz' | \
-    tar -C /usr/local/bin -zx pg_back
-  sha256sum -c - <<EOF
-3b039fea1714fdae077e7e73a25093d5acbd2a92596e362ff7c715b0471654c9 /usr/local/bin/pg_back
-EOF
-fi
-
 exec /usr/local/bin/gosu postgres:postgres /usr/local/bin/docker-entrypoint.sh postgres
