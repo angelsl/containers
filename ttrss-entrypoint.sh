590a7f709b47eb051d5a5763a0a6e042066e2653
angelsl <angelsl@in04.sg>
2023-08-21 00:34:44 +0800

    Add ttrss-entrypoint.sh

---
+++ ttrss-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source /sl/ttrss.env
+
+/opt/tt-rss/startup.sh &
+
+DST_DIR=/var/www/html/tt-rss
+while [ ! -s $DST_DIR/config.php -a -e $DST_DIR/.app_is_ready ]; do
+	echo waiting for app container...
+	sleep 3
+done
+
+sudo -E -u app /usr/bin/php82 /var/www/html/tt-rss/update_daemon2.php &
