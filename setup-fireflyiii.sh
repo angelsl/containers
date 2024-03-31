40c59d46a0a7cef14429645cb94d3e82f715bae9
angelsl <angelsl@in04.sg>
2024-03-31 19:07:35 +0800

    Update setup-fireflyiii.sh

--- setup-fireflyiii.sh
+++ setup-fireflyiii.sh
@@ -2,32 +2,31 @@
 
 set -euo pipefail
 
-mkdir -p "${ARG_DEST}"
+mkdir -p "${ARG_DEST:=/sl/fireflyiii-app}"
 cd "${ARG_DEST}"
 
-: "${ARG_GIT_REMOTE:=https://github.com/firefly-iii/firefly-iii.git}"
-: "${ARG_REF}"
+: "${ARG_GIT_REMOTE:=https://github.com/angelsl/firefly-iii.git}"
+: "${ARG_REF:=release}"
 
 rm -rf bootstrap/cache/*
-rm -rf vendor/
 
 if [ -d .git ]; then
   git fetch --depth 1 "${ARG_GIT_REMOTE}" "${ARG_REF}"
   git reset --hard
-  git clean -xfd -e /.env -e /storage/upload
+  git clean -xfd -e /.env -e /storage
   git checkout FETCH_HEAD
 else
   git clone --depth 1 -b "${ARG_REF}" "${ARG_GIT_REMOTE}" .
 fi
-
-composer install --no-scripts --prefer-dist --no-dev --ignore-platform-reqs
 
 if [ -n "${ARG_ENV_LINK:-}" ]; then
   rm -f .env
   ln -s "${ARG_ENV_LINK}" .env
 fi
 
-composer install --prefer-dist --no-dev --ignore-platform-reqs
+php artisan migrate --seed
+php artisan firefly-iii:decrypt-all
+php artisan cache:clear
 php artisan firefly-iii:upgrade-database
 php artisan firefly-iii:correct-database
 php artisan firefly-iii:report-integrity
