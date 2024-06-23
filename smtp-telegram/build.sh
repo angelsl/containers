a28a7de3dd2ba06ec3f37fce009f7558c70c1277
angelsl <angelsl@in04.sg>
2024-06-23 23:32:35 +0800

    Bump versions

--- smtp-telegram/build.sh
+++ smtp-telegram/build.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 git clone https://github.com/KostyaEsmukov/smtp_to_telegram.git sources
 cd sources
+git checkout 6cf83455f5e66a2288ba2133425e51976bfce1bb
 
 export ST_VERSION=`git describe --tags --always`
 export CGO_ENABLED=0 GOOS=linux
